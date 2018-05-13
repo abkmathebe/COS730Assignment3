34d5204f31ba8510f4faa615b01c94ecb6fe8c14
Sigama_Takalani <sigamatakalani@gmail.com>
2018-05-13 20:18:18 +0200

    done

--- Reporting_Server/app.js
+++ Reporting_Server/app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
-const mongo = require('mongodb');
+const MongoClient = require('mongodb');
 const assert = require('assert');
+var myParser = require("body-parser");
 
-var url = '';
+var url = "mongodb://localhost:27017/reportingDB";
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     console.log("Database created!");
+//     db.close();
+//   });
+
+//   MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("reportingDB");
+//     dbo.createCollection("reports", function(err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       db.close();
+//     });
+//   });
 
 const reportsRoutes = require('./api/reports');
 app.use('/reports', reportsRoutes);
+app.use(myParser.urlencoded({extended : true}));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//app.get('/', (req, res) => res.send('Hello World!'));
 
 module.exports = app;
